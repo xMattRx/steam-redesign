de3a4b309ea462bb98f3571887ff6a6a0a7272eb
Matthew Nascimento <matthew.nascimento@valtech.com>
2024-03-14 15:48:35 -0300

    feat: search component ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,33 @@
+import Game from "./components/atomic/molecules/game/game";
+import Search from "./components/atomic/molecules/search/search";
 import Topics from "./components/atomic/organisms/Topics/topics";
+import Footer from "./components/atomic/organisms/footer/footer";
+import Games from "./components/atomic/organisms/games/games";
 import Hero from "./components/atomic/organisms/hero/hero";
 import Navbar from "./components/atomic/organisms/navbar/navbar";
 import styles from "./page.module.scss";
+import farcry from '@/../public/img/farcry.png';
+import forza from '@/../public/img/forza.png';
+import fifa from '@/../public/img/fifa.png';
+import minecraft from '@/../public/img/minecraft.png';
 
 export default function Home() {
+ 
+  const gamesData = [
+    { name: 'Far Cry', image: farcry }, 
+    { name: 'Forza', image: forza },
+    { name: 'FIFA', image: fifa },
+    { name: 'Minecraft', image: minecraft }
+  ];
+  
   return (
     <main className={styles.main}>
-      <Navbar/>
-      <Hero/>
-      <Topics/>
+      <Navbar />
+      <Hero />
+      <Topics />
+      <Search />
+      <Games gamesData={gamesData} />
+      <Footer />
     </main>
   );
 }
